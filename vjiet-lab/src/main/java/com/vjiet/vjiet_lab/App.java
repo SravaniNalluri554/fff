591d864063a002e7e7db9222cf82ea794c0e2b6d
sravani <sravani@DESKTOP-TFML27L>
2021-12-29 16:04:53 +0530

    first commit

--- vjiet-lab/src/main/java/com/vjiet/vjiet_lab/App.java
+++ vjiet-lab/src/main/java/com/vjiet/vjiet_lab/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        First obj=new First(10,20);
+        obj.display();
+        
     }
 }
---
+++ vjiet-lab/src/main/java/com/vjiet/vjiet_lab/First.java
@@ -0,0 +1,18 @@
+package com.vjiet.vjiet_lab;
+
+public class First {
+	
+	int a;
+	int b;
+	
+     First (int a,int b)
+     {
+    	 this.a=a;
+    	 this.b=b;
+     }
+     void display()
+     {
+    	 System.out.println(a);
+    	 System.out.println(b);
+     }
+}
