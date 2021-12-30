aa779c6987724327766f043ddbb53b357da036c7
sravani <sravani@DESKTOP-TFML27L>
2021-12-30 10:06:38 +0530

    first commit

---
+++ vjiet-lab/src/main/java/pack1/solution/Vehicle.java
@@ -0,0 +1,23 @@
+package pack1.solution;
+
+public class Vehicle {
+	private int power;
+    private int suspensionHeight;
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getSuspensionHeight() {
+        return suspensionHeight;
+    }
+
+    public void setPower(final int power) {
+        this.power = power;
+    }
+
+    public void setSuspensionHeight(final int suspensionHeight) {
+        this.suspensionHeight = suspensionHeight;
+    }
+
+}
